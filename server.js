0afa916fdda6c0da971bf7a745888ec8a47ea1fb
Aidan Willett <Gamer9310@gmail.com>
2020-02-05 18:11:26 -0800

    heroku ready

--- server.js
+++ server.js
@@ -16,29 +16,8 @@ app.use(express.static("public"));
 const databaseUrl = "fitnesstracker";
 const collections = ["workouts"];
 
-// //is this needed?
-// const Fitness = require("./schema.js");
-// //--
-
-// const db = mongojs(databaseUrl, collections);
-
-// db.on("error", error => {
-//   console.log("Database Error:", error);
-// });
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", { useNewUrlParser: true });
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname + "public/index.html"));
-// });
-// app.get("/stats", (req, res) => {
-//   res.sendFile(path.join(__dirname + "public/stats.html"));
-// });
-// app.get("/exercise", (req, res) => {
-//   res.sendFile(path.join(__dirname + "public/exercise.html"));
-// });
-
-//need to add more code here
 app.use(require("./routes/api"))
 
 app.listen(PORT, () => {
